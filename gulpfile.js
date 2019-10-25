37301d3b74117381f993057d2a17c3289e4510f8
darellwest <ewere.njoagwu@gmail.com>
2019-10-25 23:58:52 +0100

    completed automatic sprite generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,50 @@
 
-const { dest, src, watch } = require('gulp'),
-	postcss = require("gulp-postcss"),
-	browserSync = require("browser-sync").create(),
-	autoprefixer = require("autoprefixer"),
-	cssvars = require("postcss-simple-vars"),
-	postcss_nested = require("postcss-nested"),
-	cssImport = require("postcss-import"),
-	plumber = require("gulp-plumber"),
-	mixin = require("postcss-mixins");
+const { dest, src, watch, series }  = require('gulp'),
+	postcss          				= require("gulp-postcss"),
+	browserSync 					= require("browser-sync").create(),
+	autoprefixer 					= require("autoprefixer"),
+	cssvars 						= require("postcss-simple-vars"),
+	postcss_nested  				= require("postcss-nested"),
+	cssImport 						= require("postcss-import"),
+	plumber 						= require("gulp-plumber"),
+	mixin 							= require("postcss-mixins"),
+	svgSprite 						= require("gulp-svg-sprite"),
+	rename 							= require("gulp-rename"),
+	del                             = require("del");
 
+	const config = {
+		mode: {
+			css: {
+				sprite: "sprite.svg",
+				render: {
+					css: {
+						template: "gulp/templates/sprite.css"
+					}
+				}
+			}
+		}
+	}
+
+function beginClean(){
+	return del(["app/temp/sprite", "app/assets/images/sprites"]);
+}
+
+function copySpriteGraphics(){
+	return src("app/temp/sprite/css/**/*.svg")
+	.pipe(dest("app/assets/images/sprites"));
+}
+
+function createSprite(){
+	return src("./app/assets/images/icons/**/*.svg")
+		.pipe(svgSprite(config))
+		.pipe(dest("./app/temp/sprite/"));
+}
+
+function copySpriteCSS(){
+	return src("app/temp/sprite/css/*.css")
+	.pipe(rename("_sprite.css"))
+	.pipe(dest("./app/assets/styles/modules"));
+}
 
 function anerror(errorInfo){
 	if(errorInfo){
@@ -35,3 +71,9 @@ function styles() {
  	.pipe(postcss([cssImport, mixin, cssvars, postcss_nested, autoprefixer]))
  	.pipe(dest("./app/temp/styles")).pipe(browserSync.stream());
 }
+
+function endClean(){
+	return del("./app/temp/sprite/");
+}
+
+exports.default = series(beginClean, createSprite, copySpriteGraphics, copySpriteCSS, endClean);
